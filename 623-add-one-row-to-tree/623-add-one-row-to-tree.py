bec375b65a0ac7c4dd60c838f57618dff19d95a1
Sahil Singh Yadav <91723844+cptSahil@users.noreply.github.com>
2022-10-05 21:39:35 +0530

    Time: 132 ms (8.18%), Space: 16.7 MB (69.24%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.py
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.py
@@ -7,41 +7,38 @@
 class Solution:
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
         #BFS 
-        
-        # if depth==None:
-        #     return 
-        # if depth==1:
-        #     node = TreeNode(val)
-        #     node.left == root
-        #     return node
-        # #depth!=1
-        # queue = [root]
-        # for i in range(depth-2):
-        #     curr_len = len(queue)
-        #     while curr_len>0:
-        #         node = queue.pop(0)
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
-        #         curr_len-=1
-        # for node in queue:
-        #     temp_left = node.left
-        #     temp_right = node.right
-        #     node.left = TreeNode(val)
-        #     node.right = TreeNode(val)
-        #     node.left.left = temp_left
-        #     node.right.right = temp_right
-        # return root
-            
-        if depth == 1:
-            return TreeNode(val,root,None)
-        elif depth==2:
-            root.left = TreeNode(val,root.left,None)
-            root.right = TreeNode(val,None,root.right)
-        else:
-            if(root.left!=None):
-                self.addOneRow(root.left,val,depth-1)
-            if(root.right!=None):
-                self.addOneRow(root.right,val,depth-1)
+        if depth==1:
+            node = TreeNode(val)
+            node.left = root
+            return node
+        #depth!=1
+        queue = [root]
+        for i in range(depth-2):
+            curr_len = len(queue)
+            while curr_len>0:
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                curr_len-=1
+        for node in queue:
+            temp_left = node.left
+            temp_right = node.right
+            node.left = TreeNode(val)
+            node.right = TreeNode(val)
+            node.left.left = temp_left
+            node.right.right = temp_right
         return root
+            
+#         if depth == 1:
+#             return TreeNode(val,root,None)
+#         elif depth==2:
+#             root.left = TreeNode(val,root.left,None)
+#             root.right = TreeNode(val,None,root.right)
+#         else:
+#             if(root.left!=None):
+#                 self.addOneRow(root.left,val,depth-1)
+#             if(root.right!=None):
+#                 self.addOneRow(root.right,val,depth-1)
+#         return root
