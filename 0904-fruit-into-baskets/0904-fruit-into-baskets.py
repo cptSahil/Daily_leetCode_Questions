fa24b74458e73ebfcd25ff23f3161320171b3d1e
Sahil Singh Yadav <91723844+cptSahil@users.noreply.github.com>
2023-02-07 23:03:13 +0530

    Time: 790 ms (98.25%), Space: 20.5 MB (28.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,20 +1,36 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        basket = {}
-        max_picked = 0
-        left = 0
+#         basket = {}
+#         max_picked = 0
+#         left = 0
         
-        for right in range(len(fruits)):
-            basket[fruits[right]] = basket.get(fruits[right],0) + 1
+#         for right in range(len(fruits)):
+#             basket[fruits[right]] = basket.get(fruits[right],0) + 1
             
-            while len(basket) > 2:
-                basket[fruits[left]] -= 1
-                if basket[fruits[left]] == 0:
-                    del basket[fruits[left]]
-                left += 1
+#             while len(basket) > 2:
+#                 basket[fruits[left]] -= 1
+#                 if basket[fruits[left]] == 0:
+#                     del basket[fruits[left]]
+#                 left += 1
                 
-            max_picked = max(max_picked, right-left+1)
+#             max_picked = max(max_picked, right-left+1)
             
-        return max_picked
+#         return max_picked
+
+        n=len(fruits)
+        i=j=m=0
+        x,y=fruits[0],-1
+        while j<n:
+            if fruits[j]!=x and y==-1:
+                y=fruits[j]
+            elif fruits[j]!=x and fruits[j]!=y:
+                x,y=fruits[j-1],fruits[j]
+                m=max(m,j-i)
+                i=j-1
+                while fruits[i-1]==x:
+                    i-=1
+            j+=1
+        m=max(m,j-i)
+        return m
         
         
