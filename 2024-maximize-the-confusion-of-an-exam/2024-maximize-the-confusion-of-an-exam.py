d5bcf929834232210b6d62d345cd1efaf792be58
Sahil Singh Yadav <91723844+cptSahil@users.noreply.github.com>
2023-07-07 19:33:39 +0530

    Time: 441 ms (56.70%), Space: 16.8 MB (55.49%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -4,13 +4,16 @@ class Solution:
         count = collections.Counter(answerKey[:k])
         
         left = 0
-        for right in range(k, len(answerKey)):
-            count[answerKey[right]] += 1
+        for i in range(k,len(answerKey)):
+            count[answerKey[i]] += 1
             
-            while min(count['T'], count['F']) > k: 
+            while min(count['T'],count['F'])>k:
                 count[answerKey[left]] -= 1
                 left += 1
+                
+            max_size = max(max_size,i - left + 1)
             
-            max_size = max(max_size, right - left + 1)
-                    
         return max_size
+    
+    
+        
